86ed6825b10472c53b5a44fc6ed5b74bba2a934d
Your Name <you@example.com>
2022-09-23 11:35:49 +1000

    test in jenkins with error.

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+port { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 
